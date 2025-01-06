af6843131af14f86e8d3ec6bf7e98ab460147d30
Kaan Özdemir <57274202+kaanzzdemir@users.noreply.github.com>
2025-01-06 23:08:25 +0300

    Kvkk + Contact ok

---
+++ src/js/pages/contact/contact.js
@@ -0,0 +1,10 @@
+// Global
+import Global from '../../base/_global';
+
+export default class ContactPage {
+  constructor() {
+    new Global();
+  }
+}
+
+new ContactPage();
---
+++ src/js/pages/kvkk/kvkk.js
@@ -0,0 +1,10 @@
+// Global
+import Global from '../../base/_global';
+
+export default class KvkkPage {
+  constructor() {
+    new Global();
+  }
+}
+
+new KvkkPage();
