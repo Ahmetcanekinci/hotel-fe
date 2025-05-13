55075a6c3a89522df5f8c3c5904a246a6a1f14a1
Ahmetcanekinci <113164701+Ahmetcanekinci@users.noreply.github.com>
2025-05-13 23:34:49 +0300

    içerik

--- src/js/pages/about-us/about-us.js
+++ src/js/pages/about-us/about-us.js
@@ -1,57 +1,12 @@
 // Global
-import Glide from '@glidejs/glide';
 import Global from '../../base/_global';
 
 export default class aboutPage {
   constructor() {
     
     new Global();
-    this.milestoneSlider();
 
   }
-
-  milestoneSlider() {
-
-    if( document.getElementById("milestone-slide") !== null ){
-
-      const milestoneSlider = new Glide("#milestone-slide", {
-        type: "carousel",
-        perView: 1,
-        focusAt: 0,
-        gap: 30,
-        animationDuration: 600,
-        animationTimingFunc: 'ease-in',
-      });
-
-      const milestoneDateSlider = new Glide("#milestone-date-slide", {
-        type: "carousel",
-        perView: 6,
-        focusAt: 0,
-        gap: 30,
-        animationDuration: 600,
-        animationTimingFunc: 'ease-in',
-        breakpoints:{
-          1201:{
-              perView: 3,
-          },
-        }
-      });
-      
-      milestoneSlider.mount();
-      milestoneDateSlider.mount();
-      
-      milestoneSlider.on('run', (e) => {
-        milestoneDateSlider.go(e.direction);
-      });
-
-      milestoneDateSlider.on('run', (e) => {
-        milestoneSlider.go(e.direction);
-      });
-  
-    }
-
-  }
-
 }
 
 new aboutPage();
